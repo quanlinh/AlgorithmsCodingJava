2fec95253006e12051807dbc944d41a3cf9635e9
QUAN THE TRAN <qtran@localhost.localdomain>
2018-09-06 18:59:27 -0700

    after careful review your application, we decide to move on with other candidates write more code than you

--- SubStringOfStringTest.java
+++ SubStringOfStringTest.java
@@ -1,5 +1,15 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SubStringOfStringTest {
+    @Test
+    void simpleTestCase() {
+        SubStringOfString subStringOfString = new SubStringOfString();
+        assertEquals(subStringOfString.strStr("mississippi","issip"),4);
+        assertEquals(subStringOfString.strStr("mississippi","a"),-1);
+        assertEquals(subStringOfString.strStr("mississippi","issi"),-1);
+        "asdfsaf".indexOf("a");
+    }
 
 }
