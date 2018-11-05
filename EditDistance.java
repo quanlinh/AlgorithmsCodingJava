957d708207e87c5d37f5639c0da688fa4a9a4289
QUAN THE TRAN <qtran88@cs.washington.edu>
2018-11-04 16:36:37 -0800

    implement edit distance, so happy :)

--- EditDistance.java
+++ EditDistance.java
@@ -15,8 +15,37 @@ public class EditDistance {
                     if (changeTwo == 2) return false;
                 }
             }
-
         }
         return true;
+    }
+
+    public int minimumEditDistance(String m,String n) {
+        int rows = m.length(), cols = n.length();
+        int[][] editTable = new int[m.length() + 1][n.length() + 1];
+        // Initialization
+        initialize(editTable,rows,cols);
+        for(int r = 1; r <= rows; r++ ) {
+            for (int c = 1; c <= cols; c++) {
+                int insertCost = editTable[r-1][c] + 1;
+                int deleteCost = editTable[r][c-1] + 1;
+                int subCost =  editTable[r-1][c-1];
+                if(m.charAt(r-1) != n.charAt(c-1)) {
+                    subCost += 1;
+                }
+                int minCost = Math.min(Math.min(insertCost,deleteCost),subCost);
+                editTable[r][c] = minCost;
+            }
+        }
+        return editTable[rows][cols];
+    }
+
+    private void initialize(int[][] editTable, int rows, int cols) {
+        for (int r = 0; r <= rows; r++) {
+            editTable[r][0] = r;
+        }
+        // skip one [0,0]
+        for(int c = 1; c <= cols; c++) {
+            editTable[0][c] = c;
+        }
     }
 }
--- EditDistanceTest.java
+++ EditDistanceTest.java
@@ -3,15 +3,22 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EditDistanceTest {
-@Test
-    void testOneEditAway(){
-    EditDistance editDistance = new EditDistance();
-    assertTrue(editDistance.oneEditAway("pale", "ple"));
-    assertTrue(editDistance.oneEditAway("pales", "pale"));
-    assertTrue(editDistance.oneEditAway("pale", "bale"));
-    assertFalse(editDistance.oneEditAway("bake", "pale"));
-    assertTrue(editDistance.oneEditAway("pale", "pale"));
-    assertFalse(editDistance.oneEditAway("paleee", "pale"));
-    assertFalse(editDistance.oneEditAway("pa", "pale"));
-}
+    @Test
+    void testOneEditAway() {
+        EditDistance editDistance = new EditDistance();
+        assertTrue(editDistance.oneEditAway("pale","ple"));
+        assertTrue(editDistance.oneEditAway("pales","pale"));
+        assertTrue(editDistance.oneEditAway("pale","bale"));
+        assertFalse(editDistance.oneEditAway("bake","pale"));
+        assertTrue(editDistance.oneEditAway("pale","pale"));
+        assertFalse(editDistance.oneEditAway("paleee","pale"));
+        assertFalse(editDistance.oneEditAway("pa","pale"));
+    }
+    @Test
+    void testMinimumEditDistance() {
+        EditDistance editDistance = new EditDistance();
+        assertEquals(editDistance.minimumEditDistance("horse","ros"),3);
+        assertEquals(editDistance.minimumEditDistance("intention","execution"),5);
+//        assertEquals(editDistance.minimumEditDistance("horse","ros"),3);
+    }
 }
